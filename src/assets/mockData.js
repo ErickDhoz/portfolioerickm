1f5f96c781c67aedcee982d74b1f0748f2dd758a
erickDhoz <dex4664@hotmail.com>
2023-06-29 11:32:57 -0500

    Se añadieron varios proyectos

--- src/assets/mockData.js
+++ src/assets/mockData.js
@@ -1,28 +1,16 @@
-
+import imageHerosApp from '../assets/image/AppHeroes.jpg';
 
 const mockData=[
     {
-        title:"Mi Blog",
-        image:'https://erickdhoz.github.io/ErickDhoz/imagenes/MiBlog.png',
-        link: "https://altaguajira.netlify.app/", 
-
-    },
-    {
-        title:"Contador de click",
-        image:'https://erickdhoz.github.io/ErickDhoz/imagenes/Contador de cliks.png',
+        title:"Journal APP",
+        image:'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/82efc374-4b15-42f2-a2cd-1d6905de2685/dg16v5o-c594c7fe-d9f3-4bcf-9b69-1835a29e7e56.jpg/v1/fill/w_1280,h_720,q_75,strp/journalapp_by_dex4664_dg16v5o-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvODJlZmMzNzQtNGIxNS00MmYyLWEyY2QtMWQ2OTA1ZGUyNjg1XC9kZzE2djVvLWM1OTRjN2ZlLWQ5ZjMtNGJjZi05YjY5LTE4MzVhMjllN2U1Ni5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.c1pnnCDZYdgr3BD0u4uZTIIVRyu9CuoTMjXBD6wbztY',
         link: "https://contadordeclics.netlify.app/",
 
     },
     {
-        title:"Calculadora Js",
-        image:'https://erickdhoz.github.io/ErickDhoz/imagenes/CalcuadoraJS.png',
-        link: "https://calculadoramathjs.netlify.app/",
-
-    },
-    {
-        title:"Lista de Tareas",
-        image:'https://erickdhoz.github.io/ErickDhoz/imagenes/ListaDeTareas.png',
-        link: "https://listadetareas-js.netlify.app/",
+        title:"App Heros",
+        image:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/82efc374-4b15-42f2-a2cd-1d6905de2685/dg16ur9-2f2915a2-3c12-40ec-86e2-199373bb34ac.jpg/v1/fill/w_1192,h_670,q_70,strp/appheroes_by_dex4664_dg16ur9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvODJlZmMzNzQtNGIxNS00MmYyLWEyY2QtMWQ2OTA1ZGUyNjg1XC9kZzE2dXI5LTJmMjkxNWEyLTNjMTItNDBlYy04NmUyLTE5OTM3M2JiMzRhYy5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.S6NTkhCMuZMjJUkr7A-YcPSM2-Hp_vXFhPyrDbVBKkM",
+        link: "https://appheroesdex.netlify.app/login", 
 
     },
     {
@@ -37,5 +25,21 @@ const mockData=[
         link: "https://erickdhoz.github.io/sasssiteexample/",
 
     },
+    {
+        title:"Lista de Tareas",
+        image:'https://erickdhoz.github.io/ErickDhoz/imagenes/ListaDeTareas.png',
+        link: "https://listadetareas-js.netlify.app/",
+
+    },
+    
+    {
+        title:"Calculadora Js",
+        image:'https://erickdhoz.github.io/ErickDhoz/imagenes/CalcuadoraJS.png',
+        link: "https://calculadoramathjs.netlify.app/",
+
+    },
+    
+    
+    
 ]
 export default mockData;
